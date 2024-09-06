279d2a2a47f437bbd3d7fdfaa5bbc7e89368eeee
ibrahem kezzo <ibrahemkezzo.w@gmail.com>
2024-09-06 16:53:15 +0300

    create migrations and models

--- database/migrations/2024_09_04_191604_create_tag_translations_table.php
+++ database/migrations/2024_09_04_191604_create_tag_translations_table.php
@@ -11,14 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tag_translations', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('status',['writer','admin'])->nullable();
-            $table->rememberToken();
+            $table->string('locale')->index();
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
+            $table->unique(['locale','tag_id']);
             $table->timestamps();
         });
     }
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tag_translations');
     }
 };
--- database/migrations/2024_09_04_191636_create_post_tags_table.php
+++ database/migrations/2024_09_04_191636_create_post_tags_table.php
@@ -11,15 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('post_tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('status',['writer','admin'])->nullable();
-            $table->rememberToken();
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->foreignId('tag_id')->constrained('tags')->onDelete('cascade');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
@@ -28,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('post_tags');
     }
 };
