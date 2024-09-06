998b26a915ef59bed1ebb5f74808f7d381d55265
ibrahem kezzo <ibrahemkezzo.w@gmail.com>
2024-09-07 01:52:33 +0300

    Preparing_the_models

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-class Category extends Model
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
+class Category extends Model implements  TranslatableContract
 {
-    use HasFactory;
+    use HasFactory , Translatable ,SoftDeletes;
+
+    public $translatedAttributes = ['title', 'content'];
+    protected $fillable = [ 'id', 'image', 'parent', 'created_at', 'updated_at', 'deleted_at'];
+
 }
